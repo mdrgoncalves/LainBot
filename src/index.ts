700256ff92832251ff361d6f783d7fcfd8272409
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-06-05 09:49:54 -0300

    refactor: remove unused HTTP server code from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 import dotenv from 'dotenv';
-import http from 'http';
 
 import { startListeners } from './listeners';
 
@@ -17,14 +16,3 @@ const client = new Client({
 
 startListeners(client);
 client.login(process.env.BOT_TOKEN);
-
-const PORT = process.env.PORT || 3000;
-
-http
-  .createServer((_, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bot está rodando!');
-  })
-  .listen(PORT, () => {
-    console.log(`Servidor HTTP rodando na porta ${PORT}`);
-  });
